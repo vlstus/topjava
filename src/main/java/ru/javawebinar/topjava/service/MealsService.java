13306388ed81d2374e98035375ee07805c8a5252
Vl Stus <vlstus377@gmail.com>
2020-06-05 17:06:46 +0200

    HW1_optional MealService implemented

--- src/main/java/ru/javawebinar/topjava/service/MealsService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealsService.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MockMealsMealsRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class MealsService {
@@ -15,5 +16,21 @@ public class MealsService {
 
     public List<Meal> getAllMeals() {
         return repository.getAll();
+    }
+
+    public Meal getById(int id) {
+        return repository.getById(id);
+    }
+
+    public boolean save(Meal entity) {
+        return repository.save(entity);
+    }
+
+    public boolean update(int id, LocalDateTime dateTime, String description, int calories) {
+        return repository.update(id, dateTime, description, calories);
+    }
+
+    public Meal deleteById(int id) {
+        return repository.deleteById(id);
     }
 }
