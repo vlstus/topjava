8fa94b06a358e020fb0c7a96a1f8a1d03c49dd34
Vl Stus <vlstus377@gmail.com>
2020-06-11 09:16:50 +0200

    HW1_optional MealServlet logging added

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -29,11 +29,11 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        log.debug("forward to meals");
         String action = req.getParameter("action");
         if (action == null) {
             listAllMeal(req, resp);
         } else {
+            log.debug("Performing action : " + action);
             switch (action) {
                 case "create":
                 case "update": {
@@ -59,12 +59,14 @@ public class MealServlet extends HttpServlet {
 //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("u-M-dTH:m");
         String id = req.getParameter("id");
         if (id != null) {
+            log.debug("Updating " + id);
             service.update(Integer.parseInt(id),
                     new Meal(Integer.parseInt(req.getParameter("id")),
                             LocalDateTime.parse(req.getParameter("date")),
                             req.getParameter("desc"),
                             Integer.parseInt(req.getParameter("calories"))));
         } else {
+            log.debug("Saving " + req.getParameterMap());
             service.save(new Meal(LocalDateTime.parse(req.getParameter("date")),
                     req.getParameter("desc"),
                     Integer.parseInt(req.getParameter("calories"))));
@@ -74,11 +76,13 @@ public class MealServlet extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("Deleting " + req.getParameter("id"));
         service.deleteById(Integer.parseInt(req.getParameter("id")));
         listAllMeal(req, resp);
     }
 
     private void listAllMeal(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("Dispatching to mealsList");
         req.setAttribute("mealsList", MealsUtil.filteredByStreams(service.getAllMeals()));
         req.getRequestDispatcher("views/meals.jsp").forward(req, resp);
     }
@@ -86,13 +90,17 @@ public class MealServlet extends HttpServlet {
     private void dispatchToMealsForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id");
         if (id != null) {
+            log.debug("Editing " + id);
             Meal meal = service.getById(Integer.parseInt(id));
             req.setAttribute("meal", meal);
+        } else {
+            log.debug("Creating " + req.getParameterMap());
         }
         req.getRequestDispatcher("views/meal_editor.jsp").forward(req, resp);
     }
 
     private void readById(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("Reading " + req.getParameter("id"));
         req.setAttribute("mealsList",
                 MealsUtil.filteredByStreams
                         (List.of
