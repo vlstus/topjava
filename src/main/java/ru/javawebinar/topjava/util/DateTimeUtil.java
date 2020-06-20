2b20894966b12133e1d1d74bcb26610b4ca85021
Vl Stus <vlstus377@gmail.com>
2020-06-20 08:22:13 +0200

    HW2_base DateTimeUtil isBetweenInclusive implemented

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -11,6 +11,15 @@ public class DateTimeUtil {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
     }
 
+    public static <T extends Comparable<T>> boolean isBetweenInclusive(
+            T currentDateOrTime,
+            T startDateOrTime,
+            T endDateOrTime) {
+        return currentDateOrTime.compareTo(startDateOrTime) >= 0
+                &&
+                currentDateOrTime.compareTo(endDateOrTime) <= 0;
+    }
+
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
     }
