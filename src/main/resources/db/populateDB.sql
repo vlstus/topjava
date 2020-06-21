5b981e09e6dcfbfa2946119f2ccab0ec96e637f4
Vl Stus <vlstus377@gmail.com>
2020-06-21 16:40:23 +0200

    db init scripts added

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,21 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_meals;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO user_meals(user_id, description, calories, datetime)
+VALUES (100000, 'Meal1', 200,  '2015-01-10 00:51:14'),
+       (100000, 'Meal2', 400,  '2015-01-10 00:51:14'),
+       (100001, 'Meal3', 500,  '2015-01-10 00:51:14'),
+       (100001, 'Meal4', 760,  '2015-01-10 00:51:14');
