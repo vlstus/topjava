ff55225c6a3aae2f229368db8416b1be4b74ac5e
Vl Stus <vlstus377@gmail.com>
2020-06-22 12:55:28 +0200

    tests implemented

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,50 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+    public static final int NOT_FOUND = 100;
+    public static final int MEAL_ID = START_SEQ + 2;
+    public static final int USER_ID = 100000;
+
+
+    public static final List<Meal> meal = List.of(
+            new Meal(MEAL_ID, LocalDateTime.of(2019, 5, 8, 12, 51, 14), "Meal1", 200),
+            new Meal(MEAL_ID + 1, LocalDateTime.of(2019, 8, 12, 6, 51, 14), "Meal2", 400),
+            new Meal(MEAL_ID + 2, LocalDateTime.of(2019, 2, 18, 6, 51, 14), "Meal3", 500),
+            new Meal(MEAL_ID + 3, LocalDateTime.of(2019, 4, 20, 14, 51, 14), "Meal2", 760));
+
+    public static Meal getNew() {
+        return new Meal(LocalDateTime.now(), "new", 100);
+    }
+
+    public static Meal getUpdated() {
+        Meal updated = new Meal(meal.get(0).getId(), meal.get(0).getDateTime(), meal.get(0).getDescription(), meal.get(0).getCalories());
+        updated.setDescription("Updated");
+        updated.setCalories(330);
+        return updated;
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+//        assertThat(actual).isEqualToIgnoringGivenFields(expected, "user_id");
+        assertThat(actual).isEqualToComparingFieldByField(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,91 @@
+package ru.javawebinar.topjava.service;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+import static org.junit.Assert.*;
+import static ru.javawebinar.topjava.MealTestData.*;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        // Only for postgres driver logging
+        // It uses java.util.logging and logged via jul-to-slf4j bridge
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Autowired
+    private MealRepository repository;
+
+    @Test
+    public void get() {
+        Meal meal = service.get(MEAL_ID, USER_ID);
+        assertMatch(meal, MealTestData.meal.stream()
+                .filter(testData -> testData.getId().equals(MEAL_ID))
+                .findAny()
+                .orElse(null));
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND, NOT_FOUND));
+        assertThrows(NotFoundException.class, () -> service.get(MEAL_ID, NOT_FOUND));
+    }
+
+    @Test
+    public void delete() {
+        service.delete(MEAL_ID, USER_ID);
+        assertNull(repository.get(MEAL_ID, USER_ID));
+        assertThrows(NotFoundException.class, () -> service.delete(MEAL_ID + 1, NOT_FOUND));
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+        org.assertj.core.api.Assertions.assertThat(service.getBetweenInclusive(LocalDate.of(2018, Month.JANUARY, 15), LocalDate.of(2020, Month.JANUARY, 15), USER_ID)).isNotEmpty();
+        org.assertj.core.api.Assertions.assertThat(service.getBetweenInclusive(LocalDate.now(), LocalDate.now().plus(1, ChronoUnit.MONTHS), USER_ID)).isEmpty();
+    }
+
+    @Test
+    public void getAll() {
+        assertNotNull(service.getAll(USER_ID));
+        org.assertj.core.api.Assertions.assertThat(service.getAll(NOT_FOUND)).isEmpty();
+    }
+
+    @Test
+    public void update() {
+        Meal updated = getUpdated();
+        service.update(updated, USER_ID);
+        MealTestData.assertMatch(service.get(updated.getId(), USER_ID), updated);
+    }
+
+    @Test
+    public void create() {
+        Meal aNew = getNew();
+        Meal created = service.create(aNew, USER_ID);
+        org.assertj.core.api.Assertions.assertThat(service.get(created.getId(), USER_ID))
+                .isEqualToComparingOnlyGivenFields(aNew, "description", "calories");
+    }
+}
