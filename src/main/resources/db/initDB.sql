450f775081c0fbe2db8c488d4804853632f4701c
Vl Stus <vlstus377@gmail.com>
2020-06-24 22:35:05 +0200

    init script meals index added

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -2,18 +2,20 @@ DROP TABLE IF EXISTS user_meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP INDEX IF EXISTS user_meals_idx;
+DROP INDEX IF EXISTS users_unique_email_idx;
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
 CREATE TABLE users
 (
     id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    name             VARCHAR                           NOT NULL,
-    email            VARCHAR                           NOT NULL,
-    password         VARCHAR                           NOT NULL,
-    registered       TIMESTAMP           DEFAULT now() NOT NULL,
-    enabled          BOOL                DEFAULT TRUE  NOT NULL,
-    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
+    name             VARCHAR                                         NOT NULL,
+    email            VARCHAR                                         NOT NULL,
+    password         VARCHAR                                         NOT NULL,
+    registered       TIMESTAMP           DEFAULT now()::timestamp(0) NOT NULL,
+    enabled          BOOL                DEFAULT TRUE                NOT NULL,
+    calories_per_day INTEGER             DEFAULT 2000                NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
@@ -35,4 +37,4 @@ CREATE TABLE user_meals
     CONSTRAINT user_meals_idx UNIQUE (user_id, dateTime),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-CREATE UNIQUE INDEX user_meals_idx ON user_meals (user_id, dateTime);
+CREATE UNIQUE INDEX user_meals_unique_datetime_idx ON user_meals (user_id, dateTime);
