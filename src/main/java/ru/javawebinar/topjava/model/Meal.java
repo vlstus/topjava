7c71989e839f860763d524065645eb86ff66b87a
Vl Stus <vlstus377@gmail.com>
2020-06-27 17:43:47 +0200

    JpaMealRepository implemented. Works, but must be refactored

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,48 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id"),
+        @NamedQuery(name = Meal.ALL_BETWEEN_RANGE, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id AND m.dateTime >= :startDateTime AND m.dateTime<:endDateTime")
+})
+@Entity
+@Table(name = "meals",
+        uniqueConstraints =
+                {@UniqueConstraint(columnNames = {"user_id", "date_time"},
+                        name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.delete";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String ALL_BETWEEN_RANGE = "Meal.getAllBetweenRange";
+
+    @Column(name = "date_time", nullable = false, unique = true, columnDefinition = "timestamp default now()")
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotNull
+    @Length(min = 5, max = 100)
     private String description;
 
+    @Column(name = "calories", nullable = false, columnDefinition = "int default 100")
+    @Range(min = 10, max = 2000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "user_id",
+            referencedColumnName = "id"
+    )
     private User user;
 
     public Meal() {
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,37 +1,66 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional()
 public class JpaMealRepository implements MealRepository {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        User currentUser = em.getReference(User.class, userId);
+        if (meal.isNew()) {
+            meal.setUser(currentUser);
+            em.persist(meal);
+            return meal;
+        } else {
+            meal.setUser(currentUser);
+            if (meal.getUser().id() != userId) {
+                throw new IllegalArgumentException("Meal doesn't belong to user with id " + userId);
+            }
+            return em.merge(meal);
+        }
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(Meal.DELETE)
+                .setParameter("id", id)
+                .setParameter("user_id", userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal mealWithId = em.find(Meal.class, id);
+        return mealWithId != null && mealWithId.getUser().id() == userId ? mealWithId : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(Meal.ALL_SORTED)
+                .setParameter("user_id", userId)
+                .getResultList();
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return em.createNamedQuery(Meal.ALL_BETWEEN_RANGE)
+                .setParameter("user_id", userId)
+                .setParameter("startDateTime", startDateTime)
+                .setParameter("endDateTime", endDateTime)
+                .getResultList();
     }
 }
