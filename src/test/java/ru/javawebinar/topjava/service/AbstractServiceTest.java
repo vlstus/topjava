b7633efeed4666b88cc50871fb16a2c70db27ade
Vl Stus <vlstus377@gmail.com>
2020-07-22 19:46:28 +0200

    disabled session and second level cache for tests

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -17,8 +17,8 @@ import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
 
 @ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
+        "classpath:spring/spring-app-test.xml",
+        "classpath:spring/spring-db-test.xml"
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
