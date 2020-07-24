7bac202b51e89749ebe85b05481224cf93cde3f0
Vl Stus <vlstus377@gmail.com>
2020-07-24 20:17:17 +0200

    RootControllerTest meals.jps test added
    ResourceControllerTest css test added

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.jupiter.api.Test;
+
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    void getStyleSheets() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("text/css"));
+    }
+}
--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,13 +2,18 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
+import static ru.javawebinar.topjava.MealTestData.MEALS;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
@@ -25,6 +30,34 @@ class RootControllerTest extends AbstractControllerTest {
                             @Override
                             public void assertion(List<User> actual) throws AssertionError {
                                 USER_MATCHER.assertMatch(actual, ADMIN, USER);
+                            }
+                        }
+                ));
+    }
+
+    @Test
+    void testMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<MealTo>>() {
+
+                            @Override
+                            public void assertion(List<MealTo> actual) throws AssertionError {
+                                MEAL_MATCHER.assertMatch(getEntities(actual), MEALS);
+                            }
+
+                            private List<Meal> getEntities(List<MealTo> transferObjects) {
+                                return transferObjects.stream()
+                                        .map(transferObject ->
+                                                new Meal(transferObject.getId(),
+                                                        transferObject.getDateTime(),
+                                                        transferObject.getDescription(),
+                                                        transferObject.getCalories()))
+                                        .collect(Collectors.toList());
                             }
                         }
                 ));
