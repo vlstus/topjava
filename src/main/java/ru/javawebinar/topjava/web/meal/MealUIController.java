78583d2ea53849013aef929e30526c84ac1a1a39
Vladyslav Stusiak <vlstus377@gmail.com>
2020-08-10 23:39:54 +0200

    Binding error display added. Get by id method added

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -2,14 +2,18 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/profile/meals")
@@ -22,6 +26,12 @@ public class MealUIController extends AbstractMealController {
     }
 
     @Override
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Meal get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @Override
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
@@ -30,12 +40,19 @@ public class MealUIController extends AbstractMealController {
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void createOrUpdate(@RequestParam @Valid MealTo mealTo) {
+    public ResponseEntity<String> createOrUpdate(@Valid MealTo mealTo, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorFieldsMsg = bindingResult.getFieldErrors().stream()
+                    .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
+                    .collect(Collectors.joining("<br>"));
+            return ResponseEntity.unprocessableEntity().body(errorFieldsMsg);
+        }
         if (mealTo.isNew()) {
             super.create(mealTo);
         } else {
             super.update(mealTo, mealTo.id());
         }
+        return ResponseEntity.ok().build();
     }
 
     @Override
