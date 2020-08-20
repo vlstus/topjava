5b61ccdfc780c23fe3dc730354a5099e7d051e63
Vladyslav Stusiak <vlstus377@gmail.com>
2020-08-20 11:39:21 +0200

    ugly solution to make hibernate validator work before invoking controller method and before saving to database.
    getByEmail is no longer transactional

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.User;
 
@@ -15,6 +16,8 @@ public interface CrudUserRepository extends JpaRepository<User, Integer> {
     @Query("DELETE FROM User u WHERE u.id=:id")
     int delete(@Param("id") int id);
 
+    //https://stackoverflow.com/questions/62896519/putting-database-call-inside-isvalid-method-of-custom-constraintvalidator-is
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     User getByEmail(String email);
 
     //    https://stackoverflow.com/a/46013654/548473
